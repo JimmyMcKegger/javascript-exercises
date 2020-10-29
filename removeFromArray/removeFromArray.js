ffa04f8036e0a609685bbc1f648129582f46810d
JimmyMcKegger <57632668+JimmyMcKegger@users.noreply.github.com>
2020-10-29 00:19:57 +0000

    solution

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(argOne, argTwo, argThree) {
+    let arr = arguments[0];
+    for (let i = 1; i < arguments.length; i++){
+        if (arr.includes(arguments[i])){
+            target = arr.indexOf(arguments[i]);
+            arr.splice(target, 1);
+        }
+    }
+    return arr;
 }
 
 module.exports = removeFromArray
