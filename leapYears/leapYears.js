01b2b2a5d37e9b323876a4bbef87f31890f79725
JimmyMcKegger <57632668+JimmyMcKegger@users.noreply.github.com>
2020-11-02 22:09:21 +0000

    leap Solution

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(x) {
+    if (arguments[0] % 4 == 0) {
+        if (arguments[0] % 100 == 0 && arguments[0] % 400 == 0) {
+            return true;
+        } else if (arguments[0] % 100 == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
 }
 
 module.exports = leapYears
