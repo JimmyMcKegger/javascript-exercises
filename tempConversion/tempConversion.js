d2cdc4f7d389e91e76018b97dc3ef921cea76ae0
JimmyMcKegger <57632668+JimmyMcKegger@users.noreply.github.com>
2020-11-03 00:34:13 +0000

    solution

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(x) {
+  let c = ((arguments[0] - 32.0) * (5 / 9));
+  c = parseFloat(c.toFixed(1));
+  return c;
 }
 
-const ctof = function() {
-
+const ctof = function(y) {
+  let f = (arguments[0] * 1.8 + 32.0);
+  f = parseFloat(f.toFixed(1));
+  return f;
 }
 
 module.exports = {
